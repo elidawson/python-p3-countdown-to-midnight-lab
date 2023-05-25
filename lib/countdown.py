5f81777c2c0d4d0968a6d59b43f99554e84f738a
Eli Dawson <edawson@alumni.berklee.edu>
2023-05-24 21:17:42 -0400

    Completed Lab

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,16 @@
-# your code goes here!
+import time
+
+def countdown(i):
+    while i >= 1:
+        print(f'{i} SECOND(S)!')
+        i -= 1
+    print('HAPPY NEW YEAR!')
+
+# countdown(10)
+
+def countdown_with_sleep(i):
+    while i >= 1:
+        print(f'{i} SECOND(S)!')
+        i -= 1
+        time.sleep(1)
+    print('HAPPY NEW YEAR!')
